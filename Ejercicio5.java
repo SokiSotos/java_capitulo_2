fcbfe73b8a8d85d7cecda7844840024d688045cd
SokiSotos <sokisotos@hotmail.com>
2022-10-11 12:05:26 +0200

    Ej5 - Conversor Pesetas

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -2,6 +2,6 @@ public class Ejercicio5 {
     public static void main (String [] args) {
     int pesetas=3320;
     int conve=pesetas/166;
-     System.out.println(pesetas + " Pesetas, equivalen a " + conve + " Euros.");
+        System.out.println(pesetas + " Pesetas, equivalen a " + conve + " Euros.");
     }
 }
