75379a6319bb09c0683ad2402c6dcab33f76027f
SokiSotos <sokisotos@hotmail.com>
2022-10-11 12:34:34 +0200

    Ej7 - Char y String

---
+++ Ejercicio7.java
@@ -0,0 +1,10 @@
+public class Ejercicio7 {
+    public static void main (String [] args) {
+    char a='S';
+    char b='O';
+    char c='T';
+    String d="O";
+    String e="S";
+        System.out.println(a + "" + b + "" + c + "" + d + "" + e);
+    }
+}
