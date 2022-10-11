41ad72dd2c208b2c22294b0f8a046136be60ae3b
SokiSotos <sokisotos@hotmail.com>
2022-10-11 10:58:29 +0200

    Ej4 - Conversor Euros

---
+++ Ejercicio4.java
@@ -0,0 +1,7 @@
+public class Ejercicio4 {
+    public static void main (String [] args) {
+    int euros=20;
+    int conve=euros*166;
+     System.out.println(euros + " Euros, equivalen a " + conve + " pesetas.");
+    }
+}
