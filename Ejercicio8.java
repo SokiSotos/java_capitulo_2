6fbe6239fde454dced50950a096840a317e37796
SokiSotos <sokisotos@hotmail.com>
2022-10-11 12:46:34 +0200

    Ej8 - Cadena Char

---
+++ Ejercicio8.java
@@ -0,0 +1,11 @@
+public class Ejercicio8 {
+    public static void main (String [] args) {
+    char a='S';
+    char b='O';
+    char c='T';
+    char d='O';
+    char e='S';
+    String cadena= "" + a + b + c + d + e;
+        System.out.println(cadena);
+    }
+}
