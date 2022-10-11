d96fa9ffa33a5b1f1740045032df751c8cc55c30
SokiSotos <sokisotos@hotmail.com>
2022-10-11 09:23:16 +0200

    Ej1 - Operadores

---
+++ Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+    public static void main (String [] args) {
+    int x=144;
+    int y=999;
+    float div=(float)x/(float)y;
+        System.out.println(x + " + " + y + " = " + (x+y));
+        System.out.println(x + " - " + y + " = " + (x-y));
+        System.out.println(x + " * " + y + " = " + (x*y));
+        System.out.printf(x + " / " + y + " = " + "%.2f",div);
+    }
+}
