ee0f55424a713c22b57765d2b41b3dd404f9f847
SokiSotos <sokisotos@hotmail.com>
2022-10-11 10:49:52 +0200

    Ej3 - Variables Nom,Dir,Tlf

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main (String [] args) {
+    String nombre="Jose Sotos Perez";
+    String direccion="Mi Casa";
+    int tlf=666555444;
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Mi dirección es: " + direccion);
+        System.out.println("Mi telefono es: " + tlf);
+    }
+}
