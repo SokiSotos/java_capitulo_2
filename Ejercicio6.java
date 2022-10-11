de54a8ad6b9b342dcc5506c307fbda2c08ea99d9
SokiSotos <sokisotos@hotmail.com>
2022-10-11 13:26:54 +0200

    Ej6 - Factura

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -2,6 +2,6 @@ public class Ejercicio6 {
     public static void main (String [] args) {
     int base=500;
     double total=(double)base/1.21;
-        System.out.printf("Mi base imponible es de: " + base + " Euros, por lo que el total de la factura es de: " + "%.6s",total + " Euros");
+        System.out.printf("Mi base imponible es de: " + base + " Euros, por lo que el total de la factura es de: " + "%.2f Euros",total);
     }
 }
